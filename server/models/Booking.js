8b3ec708cef763b74fd9dce41c9ade9b5eddd25e
Prakash0553 <prakashgiri20540811@gmail.com>
2026-07-22 17:32:13 +0545

    booking model updated

--- server/models/Booking.js
+++ server/models/Booking.js
@@ -6,7 +6,7 @@ const bookingSchema = new mongoose.Schema(
         show: {type: String, required: true, ref: 'Show'},
         amount: {type: Number, required:true,},
         bookedSeats: {type: Array, required: true},
-        ispaid: {type: Boolean, required: true},
+        isPaid: {type: Boolean, required: true},
         paymentLink: {type: String}
     },
     {
